6383a14c092dc7fadb695fdddde22d9bb4bcf005
José Vitor <49981381+jvitor7085@users.noreply.github.com>
2022-10-25 22:08:39 -0300

    Add files via upload

---
+++ exemploEnviaTecladoMultithreading/Servidor.java
@@ -0,0 +1,66 @@
+package exemploEnviaTecladoMultithreading;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Servidor implements Runnable {
+	public Socket cliente;
+
+	public Servidor(Socket cliente) {
+		this.cliente = cliente;
+	}
+
+	public static void main(String[] args) throws IOException {
+
+		// Cria um socket na porta 12345
+		ServerSocket servidor = new ServerSocket(12345);
+		System.out.println("Porta 12345 aberta!");
+
+		// Aguarda alguém se conectar. A execução do servidor
+		// fica bloqueada na chamada do método accept da classe
+		// ServerSocket. Quando alguém se conectar ao servidor, o
+		// método desbloqueia e retorna com um objeto da classe
+		// Socket, que é uma porta da comunicação.
+		System.out.println("Aguardando conexão do cliente...");
+
+		while (true) {
+
+			Socket cliente = servidor.accept();
+
+			// Cria uma thread do servidor para tratar a conexão
+			Servidor tratamento = new Servidor(cliente);
+
+			Thread t = new Thread(tratamento);
+
+			// Inicia a thread para o cliente conectado
+			t.start();
+		}
+	}
+
+	/*
+	 * A classe Thread, que foi instancia no servidor, implementa Runnable. Então
+	 * você terá que implementar sua lógica de troca de mensagens dentro deste
+	 * método 'run'.
+	 */
+	public void run() {
+		System.out.println("Nova conexao com o cliente " + this.cliente.getInetAddress().getHostAddress());
+
+		try {
+			Scanner s = null;
+			s = new Scanner(this.cliente.getInputStream());
+
+			// Exibe mensagem no console
+			while (s.hasNextLine()) {
+				System.out.println(s.nextLine());
+			}
+
+			// Finaliza objetos
+			s.close();
+			this.cliente.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
